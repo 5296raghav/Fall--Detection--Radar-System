95971e1f4a25726ed736bcb5dddbc97ee4b48a32
5296raghav <5296raghav@gmail.com>
2019-08-21 21:03:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -169,61 +169,78 @@ class MainClass:
         timeString = time.strftime(u"%Y%m%d-%H%M%S")
         #data1 = pd.read_csv(self.radar_data_dir + timeString + '_' + self.radar_file_name + '_.csv') 
         data1 = pd.read_csv('20190817-162913_radar_data_.csv')
-        data1.dtypes
-        rd1_data = data1.iloc[:,0:-1]
+	data1.dtypes
+
+        rd1_data = data1.iloc[:,19:-1]
         a = np.asarray(rd1_data)
-#print(np.shape(a))
+
+# print(np.shape(a))
+        print(complex(a[0][1:][0]).real)
+# a = a[:][1:] # skipping the timestamp
         arr = []
-#print(type(a),len(a),len(a[0]))
-        i = 0
+	#print(type(a),len(a),len(a[0]))
         for v in a:
-            arr.append([])
-            for x in v:
-                arr[i].append(abs(complex(x)))
-        i+=1
-#print(arr)
-#df=pd.DataFrame(arr)
-#print(df)
+		row_arr = []
+		for x in v:
+			row_arr.append(complex(x).real)
+			arr.append(row_arr)
+	 print(len(arr), len(arr[0]))
+	avg = np.mean(arr, axis=0)
+	# print(avg)
+        print(np.shape(avg))
 
-        #TTS = 0
-        #TTE = 15
-        #FR = 200
-        #CRD = 4*FR
-        #[ROW, COL] = np.shape(arr)
+        # new_avg = avg.reshape(1,np.shape(avg)[0])
 
-        #distance = [(i+20)*0.0535 for i in range(COL)]
-#print(distance)
-#print(np.shape(distance))
+        new_avg = avg.reshape((1,) + avg.shape)
+
+        #print(new_avg, np.shape(new_avg))
 
 
-        avg = np.mean(arr, axis=0)
-#print(avg)
-#print(np.shape(avg))
-        new_avg = avg.reshape(1,188)
-#print(new_avg, np.shape(new_avg))
 
-        radardata = np.subtract(arr, avg)
-#print(radardata)
-#print(np.shape(radardata))
+         radardata = np.subtract(arr, avg)
+
+         #print(radardata)
+
+         #print(np.shape(radardata))
+
+
+
 
 
         s_d = radardata.sum(axis = 1)
-#print(s_d)
-#print(np.shape(s_d))
-#S_d = s_d.reshape(3037,1)
-#print(S_d, np.shape(S_d))
 
-        s_d_0 = s_d[len(s_d)-1+1-3000:]
+        print(s_d)
 
-#print(s_d_0, np.shape(s_d_0))
+        print(np.shape(s_d))
 
-        in_data = np.transpose(s_d_0)
-#prit(in_data)
-#print(np.shape(in_data))
-        new_data = in_data.reshape(1,3000)
-        print(new_data, np.shape(new_data))
-    
-        print('Data Cleaned....')
+        #S_d = s_d.reshape(3037,1)
+
+        #print(S_d, np.shape(S_d))
+
+
+
+       s_d_0 = s_d[len(s_d)-1+1-3000:]
+
+
+
+       #print(s_d_0, np.shape(s_d_0))
+
+
+
+      in_data = np.transpose(s_d_0)
+
+      #print(in_data)
+
+      #print(np.shape(in_data))
+
+      new_data = in_data.reshape(1,3000)
+
+     print(new_data, np.shape(new_data))
+
+     # print(new_data)
+
+
+        
             
 
 if __name__ == '__main__':
